c423b527b76247124486a2df87c25d283db09759
Piyush Chauhan <83424831+cpiyush170@users.noreply.github.com>
2022-02-06 22:06:32 +0530

    Create find_height_of_a_tree.cpp

---
+++ Tree/find_height_of_a_tree.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+struct BstNode{
+    
+    int data;
+    BstNode* left;
+    BstNode* right;
+};
+// utility function to create node
+BstNode* GetNewNode(int val){
+    BstNode *newNode = new BstNode();
+    newNode->data = val;
+    newNode->left =  newNode->right = NULL;
+    return newNode;
+}
+BstNode* Insert(BstNode* root, int data){
+    
+    BstNode *newNode = GetNewNode(data);
+    // if tree is empty than assign newNode as root
+    if(root == NULL)
+    {
+        root = newNode;
+    }
+// if data at root node is less than data to be inserted
+// call Insert() recursively to put data into 
+// left subtree
+    else if( data <= root->data)
+    {
+        root->left = Insert(root->left,data);
+    }
+// or put data in right subtree
+    else
+    {
+        root->right = Insert(root->right,data);
+    }
+    return root;
+}
+
+int findHeight(BstNode* root){
+    if(root == NULL)
+    return -1;
+    else{
+        int left = findHeight(root->left);
+        int right = findHeight(root->right);
+        
+        int height = (left<right) ? right+1: left+1;
+        return height;
+    }
+}
+int main() {
+   
+   BstNode *root = NULL;
+   root = Insert(root,15);
+   root = Insert(root,10);
+   root = Insert(root,20);
+   root = Insert(root,8);
+   root = Insert(root,12);
+   root = Insert(root,17);
+   root = Insert(root,25);
+ 
+   cout << findHeight(root) <<endl;
+}
