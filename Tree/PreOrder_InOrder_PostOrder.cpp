f4974518627b3b09686378c3c62a443ebc217e13
Piyush Chauhan <83424831+cpiyush170@users.noreply.github.com>
2022-02-07 20:25:30 +0530

    Create PreOrder_InOrder_PostOrder.cpp

---
+++ Tree/PreOrder_InOrder_PostOrder.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+struct Node{
+    char data;
+    Node *left;
+    Node *right;
+};
+Node* GetNewNode(char data){
+    Node *newNode = new Node();
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+Node* Insert(Node *root, char data){
+    if(root == NULL){
+        root = GetNewNode(data);
+    }
+    else if(data <= root->data){
+        root->left = Insert(root->left,data);
+    }
+    else
+    root->right = Insert(root->right,data);
+
+    return root;
+}
+void PreOrder(Node *root){
+
+    if(root == NULL) return;
+
+    cout << root->data <<" ";
+    PreOrder(root->left);
+    PreOrder(root->right);
+}
+void InOrder(Node *root){
+    if(root == NULL) return;
+
+    InOrder(root->left); //visit left subtree
+    cout << root->data <<" "; // visit root
+    InOrder(root->right); // then visit right subtree
+}
+void PostOrder(Node *root){
+    if(root == NULL) return;
+
+    PostOrder(root->left); //visit left subtree
+    PostOrder(root->right); // then visit right subtree
+    cout << root->data <<" "; // visit root
+}
+int main() {
+    Node *root = NULL;
+    root = Insert(root,'F');
+    root = Insert(root,'D');
+    root = Insert(root,'J');
+    root = Insert(root,'B');
+    root = Insert(root,'E');
+    root = Insert(root,'G');
+    root = Insert(root,'K');
+    root = Insert(root,'A');
+    root = Insert(root,'C');
+    root = Insert(root,'I');
+    PreOrder(root);
+    cout << "\n";
+    InOrder(root);
+     cout << "\n";
+    PostOrder(root);
+
+}
