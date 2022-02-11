fb0bbe67040cd6b429cab1a7cb43c6508c9dc080
Piyush Chauhan <83424831+cpiyush170@users.noreply.github.com>
2022-02-11 15:46:42 +0530

    Create bubble_sort.cpp

---
+++ Algorithms/Sorting/bubble_sort.cpp
@@ -0,0 +1,34 @@
+/* In bubble sort, each pair of adjacent elements are compared and the elements are swapped if they are not follow the ordering rule. 
+Time Complexity - O(n^2) - in worst case */
+
+#include <iostream>
+using namespace std;
+
+// utility function to swap 
+void swap(int arr[], int nextIndex, int currIndex){
+    int temp = arr[nextIndex];
+    arr[nextIndex] = arr[currIndex];
+    arr[currIndex] = temp;
+}
+void bubble_sort(int arr[], int n){
+
+    for(int i=0; i<n-1; i++){
+        bool swapped = false;
+        for(int j=0; j<n-i-1; j++){
+            if(arr[j+1] < arr[j]){ // if next element is less than current element swap them
+                swapped = true;
+                swap(arr,j+1,j); 
+            }
+        }
+        if(!swapped) break; // if not swapped break outer loop and array is sorted
+    }
+    for(int i=0; i<=n-1; i++){
+        cout << arr[i]<<" ";
+    }
+}
+int main() {
+    
+    int arr[] = {4,3,7,1,5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    bubble_sort(arr,n);
+}
