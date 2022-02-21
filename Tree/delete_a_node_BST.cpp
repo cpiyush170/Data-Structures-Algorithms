6f90a1114fc11f0eb96e3237a0f513a0d2b71506
Piyush Chauhan <83424831+cpiyush170@users.noreply.github.com>
2022-02-21 16:15:35 +0530

    Update delete_a_node_BST.cpp

--- Tree/delete_a_node_BST.cpp
+++ Tree/delete_a_node_BST.cpp
@@ -46,35 +46,45 @@ Node* Delete(Node *root, int data){
     if(root == NULL) return root;
 
     else if(data < root->data) 
-    root->left = Delete(root->left,data);
+    root->left = Delete(root->left,data); // visit left subtree if key to find < current root node data
     else if(data > root->data)
-    root->right = Delete(root->right,data);
+    root->right = Delete(root->right,data); // else visit right subtree if key to find > current root node data
     else
-    {   // case1: if it's leaf node (no child)
+    {   // case1: if it's leaf node (no child), if the node to be deleted is the leaf node, in such case simply delete the node from tree
         if(root->left == NULL && root->right == NULL){
             delete root;
             root = NULL;
         }
-        // case 2: 1 child
-        // if left of current root is Null
+        // case 2: 1 child, If the node to deleted has single child node then follow the steps below :
+        // 1. Replace that node with its child node.
+        // 2. Remove the child node from its original position
+        
+        
+        // if left of current root is Null(empty) make current root right
         else if(root->left == NULL)
         {
             Node *temp = root;
             root = root->right;
             delete temp;
         }
-        // or right of currrent root is Null
+        
+        // or right of currrent root is Null(empty) make current root left
         else if(root->right == NULL)
         {
             Node *temp = root;
             root = root->left;
             delete temp;
         }
+        // Third Case : The node to be deleted has two children, then follow the steps below:
+        // 1. Get the inorder successor of that node
+        // 2. Replace the node with the inorder successor.
+        // 3. Remove the inorder successor from its original position
+        
         // if current root has 2 children
         else{
-            Node *temp = findMin(root->right);
-            root->data = temp->data;
-            root->right = Delete(root->right,temp->data);
+            Node *temp = findMin(root->right); // getting inorder successor
+            root->data = temp->data; // replace node to deleted value with inorder successor value
+            root->right = Delete(root->right,temp->data); // calling delete function recursively to delete inorder successor from its original position
         }
     }
     return root;
