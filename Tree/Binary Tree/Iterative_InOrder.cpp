1cee90f8866426b9cbe59865aa3cc8271cfe87bf
Piyush Chauhan <83424831+cpiyush170@users.noreply.github.com>
2022-07-15 13:02:01 +0530

    Create Iterative_InOrder.cpp

---
+++ Tree/Binary Tree/Iterative_InOrder.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void IterativeInOrder(Node *t)
+{
+    stack<Node*> st;
+
+    while(t != NULL || !st.empty())
+    {
+        if(t != NULL){
+            st.push(t);
+            t = t->left;
+        }
+        else{
+            t = st.top();
+            st.pop();
+            cout<<t->data<<" ";
+            t = t->right;
+        }
+    }
+
+}
+int main() {
+    Node *root = new Node(8);
+    root->left = new Node(3);
+    root->right = new Node(5);
+
+    root->left->left = new Node(4);
+    root->left->right = new Node(9);
+
+    root->right->left = new Node(7);
+    root->right->right = new Node(2);
+
+
+    IterativeInOrder(root);
+}
